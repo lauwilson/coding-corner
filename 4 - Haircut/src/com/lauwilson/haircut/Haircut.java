c74f530501b4c7504f668e7740260eb28d7edc0d
Wilson Lau <lauwilson90@gmail.com>
2015-04-24 00:28:50 -0700

    Haircut: First iteration complete. works only on small number values.

--- 4 - Haircut/src/com/lauwilson/haircut/Haircut.java
+++ 4 - Haircut/src/com/lauwilson/haircut/Haircut.java
@@ -1,66 +1,84 @@
 package com.lauwilson.haircut;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TreeSet;
 
+import com.lauwilson.contestIO.ContestIO;
+
 public class Haircut {
     private Scanner scan;
-    private static PrintWriter pw;
+    private BufferedWriter bw;
+    
     private int numTestCases;
     
+    public Haircut(String inputFile, String outputFile) {
+        scan = ContestIO.setInput(inputFile);
+        bw = ContestIO.setOutput(outputFile);
+    }
+    public void run() {
+        numTestCases = scan.nextInt();
+        
+        for (int i = 0; i < numTestCases; i++) {
+            // do the loopy thing
+            int numOfBarbers = scan.nextInt();
+            int customerNo = scan.nextInt();
+            int[] barberList = new int[numOfBarbers];
+            int slowestBarber = 0;
+            ArrayList<TreeSet<Integer>> availableBarbers = new ArrayList<TreeSet<Integer>>();
+            
+            // add the i-th barber's cutting speed to an array and figure out how long
+            // the slowest barber takes to complete a haircut
+            for (int j = 0; j < numOfBarbers; j++) {
+                int barberSpeed = scan.nextInt();
+                slowestBarber = Math.max(slowestBarber, barberSpeed);
+                barberList[j] = barberSpeed;
+            }
+            
+            // create an arraylist with index: 0 to (value of slowestBarber)
+            for (int j = 0; j <= slowestBarber; j++) {
+                availableBarbers.add(new TreeSet<Integer>());
+            }
+            
+            // add each barber to the first index of available barbers (time = 0)
+            for (int j = 0; j < barberList.length; j++) {
+                availableBarbers.get(0).add(j);
+            }
+            
+            // counting logic
+            int time = 0;
+            int nextCustomer = 1;
+            
+            while (nextCustomer < customerNo) {
+                while (availableBarbers.get(time).size() == 0)
+                    time = (time + 1) % (slowestBarber + 1); // CHECK LOGIC HERE MAY BE INDEX + 1 ERROR TODO
+                int nextBarber = availableBarbers.get(time).first();
+                availableBarbers.get(time).remove(nextBarber);
+                int nextTime = (time + barberList[nextBarber]) % (slowestBarber + 1);
+                availableBarbers.get(nextTime).add(nextBarber);
+                nextCustomer++;
+            }
+            // pop the last barber = the barber we want
+            while (availableBarbers.get(time).size() == 0)
+                time = (time + 1) % (slowestBarber + 1); // CHECK LOGIC HERE MAY BE INDEX + 1 ERROR TODO
+            System.out.println("Case #" + (i + 1) + ": " + (availableBarbers.get(time).first() + 1));
+        }
+    }
+    
+    public void close() {
+        if (bw != null) {
+            try {
+                bw.flush();
+                bw.close();
+            } catch (IOException e) {
+                System.err.println("IO Exception Occured: Could not close");
+            }
+        }
+    }
+    
     public static void main(String[] args) {
-        try {
-          //  scan = new Scanner(new File("input.txt"));
-            pw = new PrintWriter("output.txt", "UTF-8");
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found. Terminating program");
-            return;
-        } catch (UnsupportedEncodingException e) {
-            System.err.println("Encoding unsupported. Terminating program");
-            return;
-        }
-        
-      //  numTestCases = scan.nextInt();
-        /*int[] test = new int[200000];
-        for (int i = 0; i < 200000; i++)
-            test[i] = i;
-        for (int i = 0; i < 200000; i++)
-            System.out.println(test[i]);
-        
-        */
-        
-        List<Integer> test = new LinkedList<Integer>();
-
-        pw.close();
+        new Haircut("B-supersmall-practice.in", "small-output.txt").run();
     }
-}
-
-class CircularLinkedList {
-    class Node {
-        TreeSet<Integer> freeBarbers;
-        Node next;
-        
-        public Node() {
-            freeBarbers = new TreeSet<Integer>();
-        }
-    }
-    
-    Node head;
-    
-    
-    void add(Node n) {
-        // INSERT ADDING LOGIC
-    }
-    
-    
-    public CircularLinkedList(int max) {
-        head = new Node();
-    }
-    
 }
