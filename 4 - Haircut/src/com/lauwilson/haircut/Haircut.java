69666043f921c88255c0536af9d19620cd03a59b
Wilson Lau <lauwilson90@gmail.com>
2015-04-28 15:01:03 -0700

    completed Haircut

--- 4 - Haircut/src/com/lauwilson/haircut/Haircut.java
+++ 4 - Haircut/src/com/lauwilson/haircut/Haircut.java
@@ -2,9 +2,7 @@ package com.lauwilson.haircut;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 import com.lauwilson.contestIO.ContestIO;
 
@@ -18,53 +16,70 @@ public class Haircut {
         scan = ContestIO.setInput(inputFile);
         bw = ContestIO.setOutput(outputFile);
     }
+ 
     public void run() {
         numTestCases = scan.nextInt();
         
         for (int i = 0; i < numTestCases; i++) {
-            // do the loopy thing
-            int numOfBarbers = scan.nextInt();
-            int customerNo = scan.nextInt();
-            int[] barberList = new int[numOfBarbers];
-            int slowestBarber = 0;
-            ArrayList<TreeSet<Integer>> availableBarbers = new ArrayList<TreeSet<Integer>>();
+            int numberOfBarbers = scan.nextInt();
+            long customerNumber = scan.nextInt();
             
-            // add the i-th barber's cutting speed to an array and figure out how long
-            // the slowest barber takes to complete a haircut
-            for (int j = 0; j < numOfBarbers; j++) {
-                int barberSpeed = scan.nextInt();
-                slowestBarber = Math.max(slowestBarber, barberSpeed);
-                barberList[j] = barberSpeed;
+            int[] barberList = new int[numberOfBarbers];
+            long slowestBarber = 0;
+            
+            for (int j = 0; j < numberOfBarbers; j++) {
+                barberList[j] = scan.nextInt();
+                slowestBarber = Math.max(slowestBarber, barberList[j]);           
             }
             
-            // create an arraylist with index: 0 to (value of slowestBarber)
-            for (int j = 0; j <= slowestBarber; j++) {
-                availableBarbers.add(new TreeSet<Integer>());
+            long timeServed = findTime(customerNumber, slowestBarber, barberList);
+            
+            long customersRemaining = ((long) customerNumber) - countServedCustomers(timeServed - 1, barberList);
+            
+            for (int k = 0; k < barberList.length; k++) {
+                if (timeServed % barberList[k] == 0)
+                    customersRemaining--;
+                if (customersRemaining == 0) {
+                    System.out.println("Case #" + (i + 1) + ": " + (k+1));
+                    try {
+                        bw.write("Case #" + (i + 1) + ": " + (k+1));
+                        bw.newLine();
+                    } catch (IOException e) {
+                        System.err.println("Case #" + (i + 1) + " - ERROR: Could not write to file");
+                    }
+                    break;
+                }
             }
             
-            // add each barber to the first index of available barbers (time = 0)
-            for (int j = 0; j < barberList.length; j++) {
-                availableBarbers.get(0).add(j);
-            }
             
-            // counting logic
-            int time = 0;
-            int nextCustomer = 1;
-            
-            while (nextCustomer < customerNo) {
-                while (availableBarbers.get(time).size() == 0)
-                    time = (time + 1) % (slowestBarber + 1); // CHECK LOGIC HERE MAY BE INDEX + 1 ERROR TODO
-                int nextBarber = availableBarbers.get(time).first();
-                availableBarbers.get(time).remove(nextBarber);
-                int nextTime = (time + barberList[nextBarber]) % (slowestBarber + 1);
-                availableBarbers.get(nextTime).add(nextBarber);
-                nextCustomer++;
-            }
-            // pop the last barber = the barber we want
-            while (availableBarbers.get(time).size() == 0)
-                time = (time + 1) % (slowestBarber + 1); // CHECK LOGIC HERE MAY BE INDEX + 1 ERROR TODO
-            System.out.println("Case #" + (i + 1) + ": " + (availableBarbers.get(time).first() + 1));
         }
+        close();
+    }
+    
+    public long countServedCustomers(long time, int[] barberList) {
+        long customersServed = 0;
+        for (int i = 0; i < barberList.length; i++) {
+            customersServed += (time / barberList[i]) + 1;
+        }
+        return customersServed;
+    }
+    
+    public long findTime(long customerNo, long slowestBarber, int[] barberList) {
+        long low = -1;
+        long high = slowestBarber * customerNo;
+        
+        while (low + 1 < high) {
+            long mid = (low + high) / 2;
+            if (countServedCustomers(mid, barberList) < customerNo)
+                low = mid;
+            else
+                high = mid;    
+        }
+        return high;
+    }
+    
+    public void dbg(String s) {
+        System.out.println(s);
     }
     
     public void close() {
@@ -79,6 +94,7 @@ public class Haircut {
     }
     
     public static void main(String[] args) {
-        new Haircut("B-supersmall-practice.in", "small-output.txt").run();
+        new Haircut("B-small-practice.in", "B-small-output.txt").run();
+        new Haircut("B-large-practice.in", "B-large-output.txt").run();
     }
 }
